b1ffd54b6522e0a53e91c5f86aaf18c37c37755d
Cuke7 <cassany.louis@gmail.com>
2020-09-30 18:12:47 +0200

    Fixed cache issues

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v99";
-const DATA_CACHE_NAME = "data-cache-v99";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
