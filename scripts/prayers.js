4b0c0f875ef459e51fb2864c03959a5581312cf9
Cuke7 <cassany.louis@gmail.com>
2020-10-01 18:26:52 +0200

    Starded workink on prayers

---
+++ scripts/prayers.js
@@ -0,0 +1,145 @@
+let searching_bar = document.getElementById('search_bar');
+let shareButton = document.getElementById('butShare');
+
+let result_card_elem = document.getElementById('result_card');
+let prayer_card_elem = document.getElementById('prayer_card');
+
+let search_results = [
+	[document.getElementById('search_results1'),document.getElementById('search_results1_data')],
+	[document.getElementById('search_results2'),document.getElementById('search_results2_data')],
+	[document.getElementById('search_results3'),document.getElementById('search_results3_data')],
+	[document.getElementById('search_results4'),document.getElementById('search_results4_data')],
+	[document.getElementById('search_results5'),document.getElementById('search_results5_data')]
+]
+
+init();
+
+
+function init() {
+	searching_bar.addEventListener("keyup", function (val) {
+		//result_card_elem.style.display = 'none';
+		search_results.forEach((search_result, i) => {
+			search_result[1].innerHTML = "";
+			search_result[0].style.display = 'none';
+		});
+
+		let search_input = searching_bar.value;
+		search_input = search_input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+		search_input = search_input.toLowerCase();
+		search_input = search_input.replace(/-/g, ' ');
+
+		// If more that 2 letters in the search input
+		if (search_input.length > 2) {
+			// reset the score of each prayer
+			for (var prayer of prayers) {
+				prayer.score = 0;
+			}
+			for (var prayer of prayers) {
+				let keywords = search_input.split(" ")
+				for (var keyword of keywords) {
+					// If the keyword has at least 2 letters
+					if (keyword.length > 2) {
+						if (search_in_title_prayer(keyword, prayer)) {
+							prayer.score += 1;
+						}
+						prayer.score += count_in_text_prayer(keyword, prayer) / 5;
+					}
+				}
+			}
+			prayers_sorted = prayers.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+			update_search_results(prayers_sorted);
+			//console.log(prayers_sorted)
+		}
+	})
+
+	params = getUrlVars();
+	//console.log(params.prayer);
+	let selected_title;
+	if (params.prayer != undefined) {
+		selected_title = decodeURI(params.prayer)
+		prayer_card_elem.style.display = 'block';
+	} else {
+		prayer_card_elem.style.display = 'none';
+	}
+
+	for (var prayer of prayers) {
+		if (prayer.title == selected_title) {
+			document.getElementById('prayer_text').innerHTML = prayer.text.replace(/^		/gm, '');
+			document.getElementById('prayer_title').innerHTML = prayer.title;
+		}
+	}
+}
+
+function prayer_selected(name) {
+	insertParam(name);
+}
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+		vars[key] = value;
+	});
+	return vars;
+}
+
+function insertParam(value) {
+	key = encodeURI('prayer');
+
+	var kvp = document.location.search.substr(1).split('&');
+
+	var i = kvp.length; var x; while (i--) {
+		x = kvp[i].split('=');
+
+		if (x[0] == key) {
+			x[1] = value;
+			kvp[i] = x.join('=');
+			break;
+		}
+	}
+
+	if (i < 0) { kvp[kvp.length] = [key, value].join('='); }
+
+	//this will reload the page, it's likely better to store this until finished
+	document.location.search = kvp.join('&');
+}
+
+
+function update_search_results(prayers_sorted) {
+	//result_card_elem.style.display = 'block';
+	search_results.forEach((search_result, i) => {
+		if (prayers_sorted[i].score > 0) {
+			search_result[1].innerHTML = prayers_sorted[i].title;
+			search_result[0].name = encodeURI(prayers_sorted[i].title);
+			search_result[0].style.display = 'inherit';
+		}
+	});
+}
+
+function search_in_title_prayer(keyword, prayer) {
+	title = prayer.title.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	title = title.toLowerCase();
+	return title.includes(keyword)
+}
+
+function count_in_text_prayer(keyword, prayer) {
+	text = prayer.text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	text = text.toLowerCase();
+	var regex = new RegExp(keyword, 'g');
+	return (text.match(regex) || []).length;
+}
+
+shareButton.addEventListener('click', function () {
+	if (navigator.share) {
+		let params = getUrlVars();
+		if (decodeURI(params.prayer) == undefined) {
+			params.prayer = "Prières sur Quotidie"
+		};
+		navigator.share({
+			title: 'Quotidie ✝️',
+			text: decodeURI(params.prayer) + '\n',
+			url: window.location.href,
+		})
+			.then(() => console.log('Successful share'))
+			.catch((error) => console.log('Error sharing', error));
+	}
+})
---
+++ scripts/prayers_data.js
@@ -0,0 +1,328 @@
+let prayers = [
+	{
+		title: "",
+		text: ``
+	},
+	{
+		title: "Prière à saint Patrick",
+		text:
+			`Je me lève aujourd'hui
+		par une force puissante,
+		l'invocation de la Trinité,
+		la croyance en la Trinité,
+		la confession de l’unité
+		du Créateur du monde.
+
+		Je me lève aujourd'hui
+		par la force de la naissance du Christ et de Son Baptême,
+		la force de Sa Crucifixion et de Sa mise au tombeau,
+		la force de Sa Résurrection et de Son Ascension,
+		la force de Sa Venue au jour du Jugement.
+
+		Je me lève aujourd'hui
+		par la force des ordres des Chérubins,
+		dans l'obéissance des Anges,
+		dans le service des Archanges,
+		dans l'espoir de la Résurrection,
+		dans les prières des Patriarches,
+		dans les prédications des Prophètes,
+		dans les prédications des Apôtres,
+		dans les fidélités des Confesseurs,
+		dans l'innocence des Vierges saintes,
+		dans les actions des Hommes justes.
+
+		Je me lève aujourd'hui
+		par la force du Ciel,
+		lumière du Ciel,
+		lumière du Soleil,
+		éclat de la Lune,
+		splendeur du Feu,
+		vitesse de l'éclair,
+		rapidité du vent,
+		profondeur de la mer,
+		stabilité de la terre,
+		solidité de la pierre.
+
+		Je me lève aujourd'hui
+		par la force de Dieu pour me guider,
+		la puissance de Dieu pour me soutenir,
+		l'intelligence de Dieu pour me conduire,
+		l'œil de Dieu pour regarder devant moi,
+		l'oreille de Dieu pour m'entendre
+		la parole de Dieu pour parler pour moi,
+		la main de Dieu pour me garder,
+		le chemin de Dieu pour me précéder,
+		le bouclier de Dieu pour me protéger,
+		l'armée de Dieu pour me sauver
+		des filets des démons,
+		des séductions des vices,
+		des inclinations de la nature,
+		de tous les hommes qui me désirent du mal,
+		de loin et de près,
+		dans la solitude et dans une multitude.
+
+		J'appelle aujourd'hui toutes ces forces
+		entre moi et le mal,
+		contre toute force cruelle impitoyable
+		qui attaque mon corps et mon âme,
+		contre les incantations des faux prophètes,
+		contre les lois noires du paganisme,
+		contre les lois fausses des hérétiques,
+		contre la puissance de l'idolâtrie,
+		contre les charmes des sorciers,
+		contre toute science qui souille
+		le corps et l'âme de l'homme.
+
+		Que le Christ me protège aujourd'hui
+		contre le poison, contre le feu,
+		contre la noyade, contre la blessure,
+		pour qu'il me vienne une foule de récompenses,
+		le Christ avec moi,
+		le Christ devant moi,
+		le Christ derrière moi,
+		le Christ en moi,
+		le Christ au-dessus de moi,
+		le Christ au-dessous de moi,
+		le Christ à ma droite,
+		le Christ à ma gauche,
+		le Christ en largeur,
+		le Christ en longueur,
+		le Christ en hauteur,
+		le Christ dans le cœur
+		de tout homme qui pense à moi,
+		le Christ dans tout œil qui me voit,
+		le Christ dans toute oreille qui m'écoute.
+
+		Je me lève aujourd'hui
+		par une force puissante
+		l'invocation à la Trinité,
+		la croyance en la Trinité,
+		la confession de l'unité
+		du Créateur du monde.
+		Au Seigneur est le salut,
+		au Christ est le salut.
+		Que Ton salut, Seigneur, soit toujours avec nous.
+
+		Amen.`
+	},
+	{
+		title: "Conduis-moi, douce Lumière",
+		text:
+			`Conduis-moi, douce Lumière,
+		A travers les ténèbres qui m'encerclent.
+		Conduis-moi, Toi, toujours plus avant!
+
+		La nuit est d'encre
+		Et je suis loin de la maison:
+		Conduis-moi, Toi, toujours plus avant!
+		Garde mes pas: je ne demande pas à voir déjà
+		Ce qu'on doit voir là-bas :un seul pas à la fois
+		C'est bien assez pour moi.
+
+		Je n'ai pas toujours été ainsi
+		Et je n'ai pas toujours prié
+		Pour que Tu me conduises, Toi, toujours plus avant.
+		J'aimais choisir et voir mon sentier; mais maintenant :
+		Conduis-moi, toi, toujours plus avant!
+
+		Si longuement ta puissance m'a béni,
+		Sûrement encore elle saura me conduire toujours plus avant.
+		Par la lande et le marécage,
+		Sur le rocher abrupt et le flot du torrent
+		Jusqu'à ce que la nuit s'en soit allée...
+		Et que dans le matin sourient ces visages d'anges
+		Que j'avais aimés, il y a bien longtemps!
+
+		Conduis-moi, douce lumière,
+		Conduis-moi, toujours plus avant!
+
+		Amen.
+
+		J.H. Newman.`
+	},
+	{
+		title: "Litanies sur l'Humilité",
+		text:
+			`V. : Ô Jésus, doux et humble de cœur,
+		R. : Rendez mon cœur semblable au Vôtre.
+
+		Du désir d’être estimé, délivrez-moi Seigneur,
+		Du désir d’être affectionné, délivrez-moi Seigneur,
+		Du désir d’être recherché, délivrez-moi Seigneur,
+		Du désir d’être honoré, délivrez-moi Seigneur,
+		Du désir d’être loué, délivrez-moi Seigneur,
+		Du désir d’être préféré, délivrez-moi Seigneur,
+		Du désir d’être consulté, délivrez-moi Seigneur,
+		Du désir d’être approuvé, délivrez-moi Seigneur,
+		Du désir d’être compris, délivrez-moi Seigneur,
+		Du désir d’être visité, délivrez-moi Seigneur,
+		De la crainte d’être humilié, délivrez-moi Seigneur,
+		De la crainte d’être méprisé, délivrez-moi Seigneur,
+		De la crainte d’être rebuté, délivrez-moi Seigneur,
+		De la crainte d’être calomnié, délivrez-moi Seigneur,
+		De la crainte d’être oublié, délivrez-moi Seigneur,
+		De la crainte d’être raillé, délivrez-moi Seigneur,
+		De la crainte d’être soupçonné, délivrez-moi Seigneur,
+		De la crainte d’être injurié, délivrez-moi Seigneur,
+		De la crainte d’être abandonné, délivrez-moi Seigneur,
+		De la crainte d’être refusé, délivrez-moi Seigneur,
+		Que d’autres soient plus aimés que moi, accordez-moi, Seigneur, de le désirer,
+		Que d’autres soient plus estimés que moi, accordez-moi, Seigneur, de le désirer,
+		Que d’autres grandissent dans l’opinion et que je diminue, accordez-moi, Seigneur, de le désirer,
+		Que d’autres soient loués et que je sois oublié, accordez-moi, Seigneur, de le désirer,
+		Que d’autres soient employés et que je sois mis de côté, accordez-moi, Seigneur, de le désirer,
+		Que d’autres soient préférés en tout, accordez-moi, Seigneur, de le désirer,
+		Que d’autres soient plus saints que moi, pourvu que je le soit autant que je puis l’être, accordez-moi, Seigneur, de le désirer.
+
+		Ainsi soit-il.
+
+		Cardinal Raphaël Merry del Val (1865-1930)`
+	},
+	{
+		title: "Dieu tout-puissant et éternel, ayez pitié de Vos prêtres",
+		text:
+			`Dieu tout-puissant et éternel,
+		daignez regarder le Visage de votre Christ, l'éternel Souverain Prêtre et par amour pour Lui, ayez pitié de Vos prêtres. Souvenez-Vous, Ô Dieu miséricordieux, qu'ils ne sont que de faibles et fragiles créatures.
+		Maintenez vivant en eux le feu de votre Amour.
+
+		Gardez-les près de Vous pour que l'ennemi ne prévale pas contre eux et pour qu'ils ne soient jamais indignes de leur sublime vocation.
+
+		Ô Jésus ! Je Vous prie pour vos prêtres fidèles et fervents ; pour vos prêtres tièdes et infidèles ; pour vos prêtres qui travaillent proches de nous ou dans les missions lointaines ; pour vos prêtres qui subissent la tentation ; pour vos prêtres qui souffrent de la solitude et du délaissement ; pour vos jeunes prêtres, pour vos prêtres âgés ; pour vos prêtres infirmes ; pour vos prêtres agonisants ; pour les âmes de vos prêtres qui souffrent dans le purgatoire.
+
+		Mais surtout, je Vous recommande les prêtres qui me sont les plus chers ; le prêtre qui m'a baptisé, celui qui m'a absout de mes péchés ; les prêtres aux messes desquels j'ai assisté et qui m'ont donné votre Corps et votre Sang dans le Sainte Communion ; les prêtres qui m'ont enseigné et instruit, m'ont encouragé et conseillé ; tous les prêtres auxquels me lie une dette de gratitude. Ô Jésus !
+
+		Gardez-les près de votre Cœur et accordez-leur d'abondantes bénédictions pour le temps et pour l'éternité.
+
+		Ainsi-soit-il.
+
+		Cardinal George William Mundelein (1872-1939)`
+	},
+	{
+		title: "Prière à saint Joseph",
+		text: `
+		Ô glorieux saint Joseph, chef de la Sainte Famille de Nazareth, si zélé à pourvoir à tous ses besoins,
+		étends sur nos familles, nos proches et nos communautés ta tendre sollicitude,
+		prends sous ta conduite toutes les affaires spirituelles et temporelles qui les concernent,
+		et fais que leur issue soit pour la gloire de Dieu et le salut de nos âmes.
+
+		Amen.
+
+		Père Finet, fondateur des Foyers de Charité avec Marthe Robin`
+	},
+	{
+		title: 'Prière à saint Michel Archange',
+		text:
+			`Saint Michel Archange, défendez-nous dans le combat, soyez notre secours contre la malice et les embûches du démon.
+
+		Que Dieu exerce sur lui son empire,
+		nous vous le demandons en suppliant.
+
+		Et vous, Prince de la Milice Céleste, repoussez en enfer par la force divine Satan et les autres esprits mauvais qui rôdent dans le monde en vue de perdre les âmes.
+
+		Ainsi soit-il.
+
+		Léon XIII - 1884`
+	},
+	{
+		title: 'Prière des Francs',
+		text:
+			`Dieu Tout-Puissant et Éternel,
+		Qui avez établi l'empire des Francs, pour être dans le monde l'instrument de Vos divines volontés, Le glaive et le bouclier de Votre Sainte Église,
+		Nous Vous en prions,
+		Prévenez toujours et partout de Votre céleste lumière, les fils suppliants des Francs,
+		Afin qu'ils voient ce qu'il faut faire pour réaliser Votre règne en ce monde,
+		Et, que pour accomplir ce qu'ils auront vu,
+		Ils soient remplis de charité, de force et de persévérance.
+		Par Jésus-Christ Notre Seigneur.
+
+		Ainsi soit-il.`
+	},
+	{
+		title: "Je mets ma confiance, Vierge, en votre secours.",
+		text:
+			`Je mets ma confiance.
+		Je mets ma confiance,
+		Vierge, en votre secours,
+		Servez-moi de défense,
+		Prenez soin de mes jours ;
+
+		Et quand ma dernière heure
+		Viendra fixer mon sort,
+		Obtenez que je meure
+		De la plus sainte mort.
+
+		Sainte Vierge Marie,
+		Asile des pécheurs,
+		Prenez part, je vous prie,
+		A mes justes frayeurs :
+		Vous êtes mon refuge,
+		Votre Fils est mon Roi,
+		Mais Il sera mon Juge,
+		Intercédez pour moi.
+
+		Ah ! Soyez-moi propice
+		Avant que de mourir,
+		Apaisez Sa justice,
+		Je crains de la subir ;
+		Mère pleine de zèle,
+		Protégez votre enfant,
+		Je vous serai fidèle
+		Jusqu’au dernier instant.
+
+		A dessein de vous plaire,
+		O Reine de mon coeur !
+		Je promets de rien faire
+		Qui blesse votre honneur :
+		Je veux que, par hommage,
+		Ceux qui me sont sujets,
+		En tous lieux, à tout âge,
+		Prennent vos intérêts.
+
+		Voyez couler mes larmes,
+		Mère du bel Amour ;
+		Finissez mes alarmes
+		Dans ce mortel séjour :
+		Venez rompre ma chaîne,
+		Pour m’approcher de vous,
+		Aimable Souveraine,
+		Que mon sort serait doux !
+
+		Vous êtes, Vierge Mère,
+		Après Dieu, mon support ;
+		Je sais qu’Il est mon Père,
+		Mais vous êtes mon fort :
+		Faites que dans la gloire,
+		Parmi les bienheureux,
+		Je chante la victoire
+		Du Monarque des cieux.
+
+		« Les Oeuvres du Bx de Montfort »`
+	},
+	{
+		title: "Prière avant l'étude",
+		text:
+			`Créateur ineffable, Vous êtes la vraie source de la lumière et de la sagesse.
+		Daignez répandre Votre clarté sur l’obscurité de mon intelligence ; chassez de moi les ténèbres du péché et de l’ignorance.
+
+		Donnez-moi : La pénétration pour comprendre, La mémoire pour retenir, La méthode et la facilité pour apprendre, La lucidité pour interpréter, Une grâce abondante pour m’exprimer, Aidez le commencement de mon travail, Dirigez en le progrès, Couronnez en la fin, Par Jésus Christ Notre Seigneur.
+
+		Amen.
+
+		Saint Thomas d'Aquin`
+	},
+	{
+		title: "Vis le jour d'aujourd'hui",
+		text:
+		`Vis le jour d’aujourd’hui, Dieu te le donne, il est à toi. Vis le en Lui. 
+		Le jour de demain est à Dieu, Il ne t’appartient pas. Ne porte pas sur demain le souci d’aujourd’hui. Demain est à Dieu, remets le Lui. 
+		Le moment présent est une frêle passerelle. Si tu le charges des regrets d’hier, de l’inquiétude de demain, la passerelle cède et tu perds pied. 
+		Le passé ? Dieu le pardonne. 
+		L’avenir ? Dieu le donne. 
+		Vis le jour d’aujourd’hui en communion avec Lui. 
+		Et s’il y a lieu de t’inquiéter pour un être aimé, regarde-le dans la Lumière du Christ ressuscité. 
+		
+		Amen.
+		
+		Soeur Odette Prévost`
+	}
+]
