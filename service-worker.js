757f0774d2dc74c56de603ed525ba1daf0b5205c
Cuke7 <cassany.louis@gmail.com>
2020-10-15 13:29:10 +0200

    Removed unneccessary logs

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 "use strict";
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v0";
-const DATA_CACHE_NAME = "data-cache-v0";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
